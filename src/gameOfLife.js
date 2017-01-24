e281f24e1ff4ed650e541c014bc90fa57963ecac
Dominik Tilp <dominik.tilp@agile-ict.com>
2017-01-24 17:07:06 +0100

    pair programming dominik & jakub 24.01.2017

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,3 +1,43 @@
-export default function gameOfLife() {
-  return true;
+var score = function(board, x, y) {
+  var lived = 0;
+  for(var a = (x - 1); a <= (x + 1); a++) {  
+      if(a === -1 || a === 8) {
+        continue;
+      }    
+      for(var b = (y - 1); b <= (y + 1); b++) {
+        if(b === -1 || b === 8 || (b === y && a === x)) {
+          continue;
+        }
+        
+        if(board[b][a] === 1) {            
+          lived = lived + 1;
+        }
+      }
+  }
+  return lived;
+}
+
+export default function gameOfLife(input) {
+  var output = [];
+
+  for(var y = 0; y < input.length; y++) {    
+    output.push([]);
+    for(var x = 0; x < input.length; x++) {
+      output[y] = output[y] || [];
+      var numberOfLived = score(input, x, y);
+      if (input[y][x] === 1 && (numberOfLived < 2)) {        
+        output[y][x] = 0;
+      } else if (input[y][x] === 1 && (numberOfLived === 2 || numberOfLived === 3)) {
+        output[y][x] = 1;
+      } else if (input[y][x] === 1 && (numberOfLived > 3)) {
+        output[y][x] = 0;
+      } else if (input[y][x] === 0 && numberOfLived === 3) {
+        output[y][x] = 1;
+      } else {
+        output[y][x] = 0;
+      }
+
+    }
+  }
+  return output;
 }
--- src/gameOfLife.spec.js
+++ src/gameOfLife.spec.js
@@ -3,7 +3,121 @@ import expect from 'expect';
 import gameOfLife from './gameOfLife.js';
 
 describe('gameOfLife', () => {
-  it('return true', () => {
-    expect(gameOfLife()).toBeTruthy();
+  it('board 8x8 is a valid board', () => {
+    var input = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];    
+    var output = gameOfLife(input);
+    expect(output).toEqual(input);
   });
+
+  it('Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.', () => {
+    var input = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    var expectedOutput = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];     
+    var output = gameOfLife(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it('Any live cell with two or three live neighbours lives on to the next generation.', () => {
+    var input = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    var expectedOutput = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];   
+    var output = gameOfLife(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it('Any live cell with more than three live neighbours dies, as if by overpopulation.', () => {
+    var input = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    var expectedOutput = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];   
+    var output = gameOfLife(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', () => {
+    var input = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    var expectedOutput = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    ];   
+    var output = gameOfLife(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+
+
 });
